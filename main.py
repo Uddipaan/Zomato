dc2de0f9e419cfd7e82dbc9931f305f05ab28e93
uddipaan <uddipaanh369@gmail.com>
2020-01-25 21:30:56 +0530

    requirements.txt is added

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     api_list = []
     review_list = []
     res_list = []
-    api_list = api_info("GENERATE AND ENTER YOUR API KEY FROM https://developers.zomato.com/api")
+    api_list = api_info("fe063c59eadd9c02ee6637e4104e769a")
     review_list = review_info()
     res_list = res_info()
